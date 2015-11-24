f3489f9dbe1ed336d01af8dd6cf0150938ff03f1
philmphoenix <jpholzmann@gmail.com>
2015-11-23 17:27:27 -0700

    Solution for chaining.

--- program.js
+++ program.js
@@ -1,19 +1,11 @@
 var _ = require("lodash");
 
-var worker = function(cities) {
-  var cityNames = Object.keys(cities);
-  var isHot = function(temp){
-    return temp > 19;
-  }
-  var hot = cityNames.filter(function (cityName){
-    var temps = cities[cityName];
-    return _.every(temps, isHot);
-  })
-
-  var warm = cityNames.filter(function (cityName){
-    var temps = cities[cityName];
-    return _.some(temps, isHot) && !_.every(temps, isHot);
-  })
-  return { hot: hot, warm: warm };
+var worker = function(words) {
+  var sortedWords = _.chain(words)
+  .sort()
+  .map(function(word) {
+    return word.toUpperCase() + "CHAINED";
+  });
+  return sortedWords;
 }
 module.exports = worker
